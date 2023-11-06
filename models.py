cfa50844579d69beb69022565291b93a4e417b62
Brian Kibet <kibetsbrian@gmail.com>
2023-11-06 10:02:42 +0300

    add employee model

--- models.py
+++ models.py
@@ -8,3 +8,16 @@ import bcrypt
 
 import re 
 db = SQLAlchemy()
+
+class Employee(db.Model):
+
+    __tablename__ = 'employees' 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80))
+    password_hash = db.Column(db.String)
+    skills = db.Column(db.String(300))
+    experience = db.Column(db.Integer)
