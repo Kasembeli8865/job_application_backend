02561e85efde7564905ef500c98115c61901276a
Brian Kibet <kibetsbrian@gmail.com>
2023-11-06 10:06:11 +0300

    add password hash

--- models.py
+++ models.py
@@ -43,3 +43,11 @@ class Employee(db.Model):
             'skills': self.skills,
             'experience': self.experience,
         }
+    
+
+    def _hash_password(self, password):
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+    
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash)
